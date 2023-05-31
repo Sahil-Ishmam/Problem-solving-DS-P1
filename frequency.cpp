070bc7c25a957e14e4ec2a2881f5e01ef73934fd
Ishmam-uu <sahiluddinishmam@gmail.com>
2023-05-31 13:20:04 +0600

    modified p1

--- frequency.cpp
+++ frequency.cpp
@@ -1,18 +1,15 @@
 #include<bits/stdc++.h>
-//#include"my_queue.h"
 using namespace std;
 int main()
 {
-    int n;
-    cin>>n;
+    int n; cin>>n;
     queue<int> q1;
     while(n--)
     {
         int val;cin>>val;
         q1.push(val);
     }
-    int m;
-    cin>>m;
+    int m; cin>>m;
     queue<int> q2;
     while(m--)
     {
@@ -22,16 +19,16 @@ int main()
     int counter[10] = {0};
     queue<int> temp1 = q1;
     queue <int> temp2 = q2;
-    while(!temp1.empty())
+    while(!q1.empty())
     {
-        int val = temp1.front();
-        temp1.pop();
+        int val = q1.front();
+        q1.pop();
         counter[val]++;
     }
-    while(!temp2.empty())
+    while(!q2.empty())
     {
-        int val = temp2.front();
-        temp2.pop();
+        int val = q2.front();
+        q2.pop();
         if(counter[val]<1)
         {
             cout<<-1<<endl;
